c0586060b020223b52d01702d944ed4195c8316c
usersstef <62706574+usersstef@users.noreply.github.com>
2020-03-26 22:18:32 +0200

    Update encrypt.ps1

--- encrypt.ps1
+++ encrypt.ps1
@@ -4,8 +4,8 @@ $env:username
 
 #cmd /c manage-bde -on D: -rp -rk "C:\" -s; manage-bde -autounlock -enable D:          # encrypt full space on hdd/ssd
 #cmd /c manage-bde -on C: -used -rp -rk "C:\" -s; manage-bde -autounlock -enable C:    # encrypt used space on hdd/ssd
-#attrib -h -r -s "C:\*.BEK"; Start-Sleep -s 1; Remove-Item "C:\*.BEK"
+attrib -h -r -s "C:\*.BEK"; Start-Sleep -s 1; Remove-Item "C:\*.BEK"
 
 #cmd /c manage-bde -protectors -add -pw C:; cmd /c manage-bde -on C: -rp -rk "C:\" -s; # encrypt with password
 
-#ls c:\; attrib -h -r -s C:\*.BEK; ls c:\
+ls c:\; attrib -h -r -s C:\*.BEK; ls c:\
