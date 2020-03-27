4f306b73a3fd729446281b89459cd557c67943f5
usersstef <62706574+usersstef@users.noreply.github.com>
2020-03-27 11:35:33 +0200

    Update run_v1.0.ps1

--- run_v1.0.ps1
+++ run_v1.0.ps1
@@ -7,7 +7,11 @@ $userlist = @("name1familyname.companyname.com",
 # Get_info
 
 foreach ($user in $userlist) {
-	powershell -ExecutionPolicy bypass -command Invoke-Command -ComputerName $user -filepath c:\xxx\script\get_info.ps1 | Select-String -Pattern "=","Link-local","Secure Boot","Automatic","Adapter","Tunnel","Media","Configuration","Ethernet","Connection","Subnet","Default","Attempting","BitLocker","Copyright","Protectors","TPM:","PCR","0," -NotMatch | Out-File "c:\xxx\usr_file\info.txt" -Append -Verbose
+	powershell -ExecutionPolicy bypass -command Invoke-Command -ComputerName $user `
+	-filepath c:\xxx\script\get_info.ps1 | Select-String -Pattern "=","Link-local", `
+	"Secure Boot","Automatic","Adapter","Tunnel","Media","Configuration","Ethernet", `
+	"Connection","Subnet","Default","Attempting","BitLocker","Copyright","Protectors", `
+	"TPM:","PCR","0," -NotMatch | Out-File "c:\xxx\usr_file\info.txt" -Append -Verbose
 	}
 
 # Remote_encrypt
@@ -19,11 +23,15 @@ foreach ($user in $userlist) {
 # Get_drive
 
 foreach ($user in $userlist) {
-	powershell -ExecutionPolicy bypass -command Invoke-Command -ComputerName $user -filepath c:\xxx\script\get_drive.ps1 | Select-String -Pattern "Attempting" -NotMatch | Out-File c:\xxx\usr_file\drive.txt -Append -Verbose
+	powershell -ExecutionPolicy bypass -command Invoke-Command -ComputerName $user `
+	-filepath c:\xxx\script\get_drive.ps1 | Select-String -Pattern "Attempting" `
+	-NotMatch | Out-File c:\xxx\usr_file\drive.txt -Append -Verbose
 	}
 
 # Time_sync
 
 foreach ($user in $userlist) {
-	powershell -ExecutionPolicy bypass -command Invoke-Command -ComputerName $user -filepath c:\xxx\script\time_set.ps1 | Select-String -Pattern "Firewall","Attempting","powershell","c:","category","error" -NotMatch | Out-File "c:\xxx\usr_file\time.txt" -Append -Verbose
+	powershell -ExecutionPolicy bypass -command Invoke-Command -ComputerName $user `
+	-filepath c:\xxx\script\time_set.ps1 | Select-String -Pattern "Firewall","Attempting", `
+	"powershell","c:","category","error" -NotMatch | Out-File "c:\xxx\usr_file\time.txt" -Append -Verbose
 	}
