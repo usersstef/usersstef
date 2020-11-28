c59f9a53a637d291c3290bba51e8d665f0415529
usersstef <62706574+usersstef@users.noreply.github.com>
2020-11-28 14:53:18 +0200

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,18 +1,18 @@
 
-## Automated web navigation
+# Automated web navigation
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium.webdriver import ActionChains
-import time, pyperclip
+import time
 
 # *** Setting paths ***
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(ChromeDriverManager().install()) # driver path
-options.add_argument(r"user-data-dir=C:\Users\Stefan\AppData\Local\Google\Chrome\User Data") # user profile path
+driver = webdriver.Chrome(ChromeDriverManager().install())  # driver path
+options.add_argument(r"user-data-dir=C:\Users\Stefan\AppData\Local\Google\Chrome\User Data")  # user profile path
 driver.maximize_window()
 
 # Get URL & close modal
@@ -21,38 +21,38 @@ close_modal = '//*[@id="at-cv-lightbox-button-holder"]/a[2]'
 driver.find_element_by_xpath(close_modal).click();time.sleep(1)
 
 # Input single field
-driver.find_element_by_xpath("//*[@id='user-message']").send_keys('The text message is inserted here successfully');time.sleep(1) # enter msg
-driver.find_element_by_xpath("//*[@id='get-input']/button").click();time.sleep(1) # show msg button
+driver.find_element_by_xpath("//*[@id='user-message']").send_keys('The text message is inserted here successfully');time.sleep(1)  # enter msg
+driver.find_element_by_xpath("//*[@id='get-input']/button").click();time.sleep(1)  # show msg button
 
 # Input two field
-driver.find_element_by_xpath("//*[@id='sum1']").send_keys('1'); time.sleep(1) # enter a field
-driver.find_element_by_xpath("//*[@id='sum2']").send_keys('2'); time.sleep(1) # enter b field
-driver.find_element_by_xpath("//*[@id='gettotal']/button").click() # get total button
-driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);");time.sleep(1)
-driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME);time.sleep(1)
+driver.find_element_by_tag_name('body').send_keys(Keys.END);time.sleep(1)
+driver.find_element_by_xpath("//*[@id='sum1']").send_keys('1'); time.sleep(1)  # enter a field
+driver.find_element_by_xpath("//*[@id='sum2']").send_keys('2'); time.sleep(1)  # enter b field
+driver.find_element_by_xpath("//*[@id='gettotal']/button").click(); time.sleep(1)  # get total button
+driver.find_element_by_tag_name('body').send_keys(Keys.HOME);time.sleep(1)
 
-# Checkbox buttons
-driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/a").click();time.sleep(1) # Input forms
-driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/ul/li[2]/a").click();time.sleep(1) # checkbox buttons
+# Checkboxes
+driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/a").click();time.sleep(1)  # Input form
+driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/ul/li[2]/a").click();time.sleep(1)  # checkbox button
 driver.find_element_by_xpath("//*[@id='isAgeSelected']").click();time.sleep(1)
-driver.find_element_by_xpath("//*[@id='check1']").click();time.sleep(1) # check all
+driver.find_element_by_xpath("//*[@id='check1']").click();time.sleep(1)  # check all
 
 # Modals
-driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[5]/a").click();time.sleep(1) # Alert & modals
-driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[5]/ul/li[2]/a").click();time.sleep(1) # Bootstrap modals
-driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div[2]/a").click();time.sleep(1) # launch multiple modals
+driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[5]/a").click();time.sleep(1)  # Alert & modals
+driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[5]/ul/li[2]/a").click();time.sleep(1)  # Bootstrap modal
+driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div[2]/a").click();time.sleep(1)  # multiple modals
 driver.find_element_by_xpath("//*[@id='myModal']/div/div/div[3]/a").click();time.sleep(1)
-driver.find_element_by_xpath("//*[@id='myModal2']/div/div/div[6]/a[1]").click();time.sleep(1) # close
+driver.find_element_by_xpath("//*[@id='myModal2']/div/div/div[6]/a[1]").click();time.sleep(1)  # close
 driver.find_element_by_xpath("//*[@id='myModal']/div/div/div[4]/a[1]").click();time.sleep(1)
 
 # Dropdown list
-driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/a").click();time.sleep(1) # main tree
-driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/ul/li[4]/a").click();time.sleep(1) # dropdown list
-Select(driver.find_element_by_xpath("//*[@id='select-demo']")).select_by_index(6);time.sleep(1) # selector
-driver.find_element_by_xpath("//*[@id='multi-select']/option[6]").click(); time.sleep(1) # option
+driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/a").click();time.sleep(1)  # main tree
+driver.find_element_by_xpath("//*[@id='treemenu']/li/ul/li[1]/ul/li[4]/a").click();time.sleep(1)  # dropdown list
+Select(driver.find_element_by_xpath("//*[@id='select-demo']")).select_by_index(6);time.sleep(1)  # selector
+driver.find_element_by_tag_name('body').send_keys(Keys.END);time.sleep(1)
+driver.find_element_by_xpath("//*[@id='multi-select']/option[6]").click(); time.sleep(1)  # option
 driver.find_element_by_xpath("//*[@id='printMe']").click(); time.sleep(1) # first selected button
-driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);");time.sleep(1)
-driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME);time.sleep(1)
+driver.find_element_by_tag_name('body').send_keys(Keys.HOME);time.sleep(1)
 
 # Range sliders
 driver.get('https://www.seleniumeasy.com/test/drag-drop-range-sliders-demo.html');time.sleep(1)
@@ -76,32 +76,32 @@ driver.find_element_by_xpath("//*[@id='contact_form']/fieldset/div[2]/div/div/in
 
 # Drag and drop
 driver.get('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
-source = driver.find_element_by_xpath("//*[@id='box1']") # Stokkolm
-target = driver.find_element_by_xpath("//*[@id='box101']") # Sweden
+source = driver.find_element_by_xpath("//*[@id='box1']")  # Stokkolm
+target = driver.find_element_by_xpath("//*[@id='box101']")  # Sweden
 ActionChains(driver).drag_and_drop(source,target).perform()
 
-source = driver.find_element_by_xpath("//*[@id='box2']") # Oslo
-target = driver.find_element_by_xpath("//*[@id='box102']") # Norway
+source = driver.find_element_by_xpath("//*[@id='box2']")  # Oslo
+target = driver.find_element_by_xpath("//*[@id='box102']")  # Norway
 ActionChains(driver).drag_and_drop(source,target).perform()
 
-source = driver.find_element_by_xpath("//*[@id='box3']") # Washington
-target = driver.find_element_by_xpath("//*[@id='box103']") # United States
+source = driver.find_element_by_xpath("//*[@id='box3']")  # Washington
+target = driver.find_element_by_xpath("//*[@id='box103']")  # United States
 ActionChains(driver).drag_and_drop(source,target).perform()
 
-source = driver.find_element_by_xpath("//*[@id='box4']") # Copenhagen
-target = driver.find_element_by_xpath("//*[@id='box104']") # Denmark
+source = driver.find_element_by_xpath("//*[@id='box4']")  # Copenhagen
+target = driver.find_element_by_xpath("//*[@id='box104']")  # Denmark
 ActionChains(driver).drag_and_drop(source,target).perform()
 
-source = driver.find_element_by_xpath("//*[@id='box5']") # Seoul
-target = driver.find_element_by_xpath("//*[@id='box105']") # South Corea
+source = driver.find_element_by_xpath("//*[@id='box5']")  # Seoul
+target = driver.find_element_by_xpath("//*[@id='box105']")  # South Corea
 ActionChains(driver).drag_and_drop(source,target).perform()
 
-source = driver.find_element_by_xpath("//*[@id='box6']") # Rome
-target = driver.find_element_by_xpath("//*[@id='box106']") # Italy
+source = driver.find_element_by_xpath("//*[@id='box6']")  # Rome
+target = driver.find_element_by_xpath("//*[@id='box106']")  # Italy
 ActionChains(driver).drag_and_drop(source,target).perform()
 
-source = driver.find_element_by_xpath("//*[@id='box7']") # Madrid
-target = driver.find_element_by_xpath("//*[@id='box107']") # Spain
+source = driver.find_element_by_xpath("//*[@id='box7']")  # Madrid
+target = driver.find_element_by_xpath("//*[@id='box107']")  # Spain
 ActionChains(driver).drag_and_drop(source,target).perform();time.sleep(1)
 print();print(" *** Script executed successfully :) *** ")
 driver.refresh();time.sleep(1);driver.close()
