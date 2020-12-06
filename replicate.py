897d6bd4621c21e0ec68f45770c18cd89af42027
usersstef <62706574+usersstef@users.noreply.github.com>
2020-12-06 20:10:19 +0200

    Update replicate.py

--- replicate.py
+++ replicate.py
@@ -1,9 +1,5 @@
 
-
 import shutil
 
 for i in range(50):
-    shutil.copyfile('D:\\miscellaneous\\__scripts\\replicate\\3_bADVANTAGE_CROSS_.mp3','D:\\miscellaneous\\__scripts\\replicate\\3_bADVANTAGE_CROSS_'+str(i+1)+'.mp3')
-    
-    
-    
+    shutil.copyfile('D:\\Scripts\\Python\\replicate\\AudioFile_.wav','D:\\Scripts\\Python\\replicate\\AudioFile_'+str(i+1)+'.wav')
