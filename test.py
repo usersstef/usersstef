ba846d75fbb1f71261eaed29fcf0869f6355edd1
usersstef <62706574+usersstef@users.noreply.github.com>
2020-12-11 18:31:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,9 @@ driver = webdriver.Chrome(ChromeDriverManager().install())                      
 options.add_argument(r"user-data-dir=C:\Users\Stefan\AppData\Local\Google\Chrome\User Data")  # user profile path
 driver.maximize_window()
 
-for draft_content, priority in zip(range(2), range(2)):
+# Generating as many draft contents are needed
+
+for draft_content, priority in zip(range(51), range(51)):
     print()
     print("DraftContent_%d" %draft_content, "Priority %d" %priority)
     driver.get('https://twitter.com/intent/follow?screen_name=seleniumeasy')
@@ -33,8 +35,10 @@ for draft_content, priority in zip(range(2), range(2)):
     time.sleep(1)  # no thanks button
 driver.close()
 
-# after above code is run, there will be to execute 3 ore 4 lines more to bulk publish the 50 or 100 draft contents :)
-ctypes.windll.user32.MessageBoxW(0, "Draft Contents generated & published", "Info", 1)
+# after above code is run, there will be to execute 4 ore 5 lines more to bulk publish the 50 or 100 draft contents :)
+#ctypes.windll.user32.MessageBoxW(0, "Draft Contents generated & published", "Info", 1)
+
+# Checking the text inside an alert message and doing something after
 
 class BootstrapAlert:
     options = webdriver.ChromeOptions()
@@ -43,9 +47,9 @@ class BootstrapAlert:
     driver.maximize_window()
 
     def __init__(self):
-        self.check_text()
+        self.check_context()
 
-    def check_text(self):
+    def check_context(self):
         self.driver.get('https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html')
         time.sleep(1)
         self.driver.find_element_by_xpath("//*[@id='normal-btn-success']").click()
@@ -58,8 +62,9 @@ class BootstrapAlert:
             print("The message is: " + alert_content)
             time.sleep(1)
         else:
+            time.sleep(1)
             self.driver.find_element_by_xpath(
-                "/ html / body / div[2] / div / div[2] / div / div[2] / div[2] / button").click()
+                "/ html / body / div[2] / div / div[2] / div / div[2] / div[2] / button").click();time.sleep(1)
 
     def exit(self):
         self.driver.close()
@@ -68,6 +73,3 @@ class BootstrapAlert:
 if __name__ == '__main__':
     WebSession = BootstrapAlert()
     WebSession.exit()
-
-
-
