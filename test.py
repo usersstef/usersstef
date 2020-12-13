db431cad2f18928ac14bcae87b90df297d944960
usersstef <62706574+usersstef@users.noreply.github.com>
2020-12-13 22:42:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ driver.maximize_window()
 
 for draft_content, priority in zip(range(2), range(2)):
     print()
-    print("DraftContent_%d" %draft_content, "Priority %d" %priority)
+    print("DraftContent_%d" %draft_content, "Priority %d" % priority)
     driver.get('https://twitter.com/intent/follow?screen_name=seleniumeasy')
     time.sleep(1.5)
     driver.find_element_by_xpath(
         "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[2]/form/div/div[1]/label/div/div[2]/div/input").send_keys(
-        'DraftContent_%d' %draft_content)
+        'DraftContent_%d' % draft_content)
     time.sleep(1)  # user
     driver.find_element_by_xpath(
         "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[2]/form/div/div[2]/label/div/div[2]/div/input").send_keys(
