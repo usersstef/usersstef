a293cb814b7ab97eef7c8ca593f6a26d2a82c956
usersstef <62706574+usersstef@users.noreply.github.com>
2021-04-21 20:04:52 +0300

    Update nonidle.py

--- nonidle.py
+++ nonidle.py
@@ -5,8 +5,7 @@ import time
 
 while True:
     win = gw.getWindowsWithTitle('ro-3106708.ad.ea.com')[0]
-    now = datetime.now()
-    run_time = now.strftime("%H:%M:%S")
+    run_time = datetime.now().strftime("%H:%M:%S")
     if True:
         win.activate(); win.maximize(); time.sleep(1); win.minimize()
         print("Checked:", run_time); print()
