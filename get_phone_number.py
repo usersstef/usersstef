ffe26a0a2807392a20e985c47c98a959c4150c90
usersstef <62706574+usersstef@users.noreply.github.com>
2021-04-30 00:46:29 +0300

    Add files via upload

---
+++ get_phone_number.py
@@ -0,0 +1,27 @@
+
+import pyperclip, re
+
+file = open('info.txt')
+numlist = list()
+for line in file:
+    phone_number = re.findall('\w+\s\w+\s\d{4}\s\d{3}\s\d{3}', file.read())
+    if len(phone_number) > 0:
+        numlist.extend(phone_number)
+print()
+print("Phone numbers are:", *numlist, sep='\n')
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
