07d93f62380264bd1caa3dd4e22422ece147f190
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-01 00:28:30 +0300

    Update get_phone_number.py

--- get_phone_number.py
+++ get_phone_number.py
@@ -4,13 +4,13 @@
 import re
 import sys
 
-sys.stdout = open('D:\Scripts\Python\phone_list.txt', 'w')
-file = open('info.txt')
+sys.stdout = open('D:\Scripts\Python\phone_list.txt', 'w') # output
+file = open('info.txt') # input
 numlist = list()
 for line in file:
-    phone_number = re.findall('\w+\s\w+\s\d{4}\-\d{3}\-\d{3}', file.read())
-    if len(phone_number) > 0:
-        numlist.extend(phone_number)
+    pattern = re.findall('\w+\s\w+\s\d{4}\-\d{3}\-\d{3}', file.read()) # regex
+    if len(pattern) > 0:
+        numlist.extend(pattern)
 print()
 print("Phone numbers are:", *numlist, sep='\n')
 print()
