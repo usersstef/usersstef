4cab46245ec20566d69197e5854cdb3a8263df55
usersstef <62706574+usersstef@users.noreply.github.com>
2021-04-30 20:52:58 +0300

    Update get_phone_number.py

--- get_phone_number.py
+++ get_phone_number.py
@@ -1,17 +1,20 @@
 
 # The text sample from info.txt file: "First Name 0722 000 111 random text Second Name 0723 222 333"
 
-import pyperclip, re
+import re
+import sys
 
+sys.stdout = open('D:\Scripts\Python\phone_list.txt', 'w')
 file = open('info.txt')
 numlist = list()
 for line in file:
-    phone_number = re.findall('\w+\s\w+\s\d{4}\s\d{3}\s\d{3}', file.read())
+    phone_number = re.findall('\w+\s\w+\s\d{4}\-\d{3}\-\d{3}', file.read())
     if len(phone_number) > 0:
         numlist.extend(phone_number)
 print()
 print("Phone numbers are:", *numlist, sep='\n')
 print()
+sys.stdout.close()
 
 
 
