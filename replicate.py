cbcd8193dff1c4f9e905d94d61a61acecd3c07fb
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-27 18:59:17 +0300

    Update replicate.py

--- replicate.py
+++ replicate.py
@@ -1,8 +1,9 @@
 
+# The script renames incremental a file in a given range
+
 import shutil
 
 for i in range(50):
     shutil.copyfile('D:\\Scripts\\Python\\replicate\\AudioFile_.wav','D:\\Scripts\\Python\\replicate\\AudioFile_'+str(i+1)+'.wav')
 
-    
-    
+ 
