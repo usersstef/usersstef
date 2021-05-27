e30b265445fc490f7d6b4470565875599fa04ebd
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-27 18:57:34 +0300

    Update nonidle.py

--- nonidle.py
+++ nonidle.py
@@ -1,12 +1,17 @@
 
+# The script perform Alt + Tab command on an open window
+
 import pygetwindow as gw
 from datetime import datetime
 import time
 
 while True:
-    win = gw.getWindowsWithTitle('ro-3106708.ad.ea.com')[0]
+    win = gw.getWindowsWithTitle('TheNameOfTheWindow')[0]
     run_time = datetime.now().strftime("%H:%M:%S")
     if True:
         win.activate(); win.maximize(); time.sleep(1); win.minimize()
         print("Checked:", run_time); print()
         time.sleep(290)
+
+        
+        
