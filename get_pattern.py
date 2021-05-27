9c105f9a4561b46e01b799aec73dcf3215a62cb0
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-27 22:26:39 +0300

    Update get_pattern.py

--- get_pattern.py
+++ get_pattern.py
@@ -5,26 +5,15 @@
 import re
 import sys
 
-#sys.stdout = open('D:\Scripts\Python\output_list.txt', 'w') # output
-file = open('info.txt') # input
+#sys.stdout = open('D:\Scripts\Python\output_list.txt', 'w') # write output in a file
+file = open('data.txt') # input
 numlist = list()
 for line in file:
-    pattern = re.findall('\w+ ?\w+ ?\w+\s+\w+(?:(?:\s\d+-\d+-\d+)?\s+[A-Za-z0-9.+_*-]+@[a-z0-9.+_-]+\.[a-z]+|\s\d+-\d+-\d+)', file.read())
+    pattern = re.findall('\w+\s+\w+\w+ ?\w+ ?(?:(?:\s\d+-\d+-\d+)?\s+[A-Za-z0-9.+_*-]+@[a-z0-9.+_-]+\.[a-z]+|\s\d+-\d+-\d+)', file.read())
     if len(pattern) > 0:
         numlist.extend(pattern)
 print()
 print("Searched items are:", *numlist, sep ='\n')
 print()
 #sys.stdout.close()
-
-
-
-
-
-
-
-
-
-
-
 
