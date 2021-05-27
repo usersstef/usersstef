eabb61ee71c8b567a421a24e7f5ce984f202ad8b
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-27 22:30:05 +0300

    Update get_pattern.py

--- get_pattern.py
+++ get_pattern.py
@@ -1,6 +1,6 @@
 
 # The regex matches for 'First Name1 Name2 Name3 0723-111-222 email@address1.com' following next pattern:
-# <FirstName> <SecondName> <OptionalName> <OptionalName> (OptionalPhoneNumber | OptionalEmailAddress)
+# <FirstName> <SecondName> <OptionalName> <OptionalName> (OptionalPhoneNumber or OptionalEmailAddress)
 
 import re
 import sys
