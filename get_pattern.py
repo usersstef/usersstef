019d7a04be9ed9e2cb4a42855c759c89d241304b
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-28 12:52:21 +0300

    Update get_pattern.py

--- get_pattern.py
+++ get_pattern.py
@@ -6,7 +6,7 @@ import re
 import sys
 
 #sys.stdout = open('D:\Scripts\Python\output_list.txt', 'w') # write output in a file
-file = open('data.txt') # input
+file = open('data.txt') # source file
 numlist = list()
 for line in file:
     pattern = re.findall('\w+\s+\w+\w+ ?\w+ ?(?:(?:\s\d+-\d+-\d+)?\s+[A-Za-z0-9.+_*-]+@[a-z0-9.+_-]+\.[a-z]+|\s\d+-\d+-\d+)', file.read())
