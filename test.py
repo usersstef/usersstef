7454eba968ed5ff4939bf997fc26faf8b24fb275
usersstef <62706574+usersstef@users.noreply.github.com>
2021-05-31 10:34:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,29 +4,39 @@
 from selenium import webdriver
 import time
 
-print();print("  Initializing test environment...")
+print()
+print("  Initializing test environment...")
 
-driver = webdriver.Chrome(executable_path=r'D:\Scripts\Python\Web_automation\chromedriver.exe')
+driver = webdriver.Chrome(
+    executable_path=r"D:\Scripts\Python\Web_automation\chromedriver.exe"
+)
 driver.maximize_window()
 
-print();print("--- Testing process started ---")
+print()
+print("--- Testing process started ---")
 
-# Loop to fill in incremented values
+# Loop to fill incremented values
 
 for draft_content, priority in zip(range(2), range(2)):
     print()
     print("DraftContent_%d" % draft_content, "Priority %d" % priority)
-    driver.get('https://twitter.com/intent/follow?screen_name=seleniumeasy')
+    driver.get("https://twitter.com/intent/follow?screen_name=seleniumeasy")
     time.sleep(1.5)
     driver.find_element_by_xpath(
-        "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[2]/form/div/div[1]/label/div/div[2]/div/input").send_keys('DraftContent_%d' % draft_content)
+        "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[2]/form/div/div[1]/label/div/div[2]/div/input"
+    ).send_keys("DraftContent_%d" % draft_content)
     time.sleep(1)  # user
     driver.find_element_by_xpath(
-        "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[2]/form/div/div[2]/label/div/div[2]/div/input").send_keys(priority)
+        "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[2]/form/div/div[2]/label/div/div[2]/div/input"
+    ).send_keys(priority)
     time.sleep(1)  # pass
-    driver.find_element_by_xpath("//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/span/span/span").click()
+    driver.find_element_by_xpath(
+        "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/span/span/span"
+    ).click()
     time.sleep(1)  # no thanks button
-print();print(" *** Test executed successfully :) *** ");print()
+print()
+print(" *** Test executed successfully :) *** ")
+print()
 driver.close()
 
 # Read & validate the text of an alert message and doing something after
