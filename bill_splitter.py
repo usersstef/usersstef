b0946cbc1e605a94bffacbdc2761feae41308baa
usersstef <62706574+usersstef@users.noreply.github.com>
2021-06-01 11:16:05 +0300

    Update bill_splitter.py

--- bill_splitter.py
+++ bill_splitter.py
@@ -9,14 +9,14 @@ for line in file:
     line_list = re.findall('\d*?\.\d+', file.read())
     if len(line_list) > 0:
         numlist.extend(line_list)
-print();print("All the common costs until now:", numlist);print()
-print("*************************************");print()
+print();print("All the common costs until now:", *numlist, sep ='\n');print()
 
 total = 0
 for sum in numlist:
     total += float(sum)
-print("The common Total until now:    ", total);print();print("The common Total divided on 3: ", float(total/3))
-print();input(" ")
+print("The common Total until now:    ", total);print()
+print("The common Total divided on 3: ", float(total/3))
+print();input("")
 
 
 
