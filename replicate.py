2f851163092eb121ae1f464ab1e060db4a21f772
usersstef <62706574+usersstef@users.noreply.github.com>
2021-06-11 10:52:51 +0300

    Update replicate.py

--- replicate.py
+++ replicate.py
@@ -1,5 +1,5 @@
 
-# Generate files with incremented names
+# Generate files with incremented names, just copy the script in the same folder with file to copy
 # If the script is run from other path, use the absolute path of the file like the below example:
 # shutil.copyfile('D:\\Scripts\\Python\\replicate\\53_CBank_.wav','D:\\Scripts\\Python\\replicate\\53_CBank_'+str(i+1)+'.wav')
 
