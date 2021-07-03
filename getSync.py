a931b9870afbe7f92df55c97582bd211d56305ad
usersstef <62706574+usersstef@users.noreply.github.com>
2021-07-03 16:02:06 +0300

    Add files via upload

---
+++ getSync.py
@@ -0,0 +1,55 @@
+
+# Automated actions with loops & functions
+# Block comment: Shift + Alt + A
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+# Initializing test environment
+
+print();print("  Initializing test environment...")
+
+driver = webdriver.Chrome(
+    executable_path=r"D:\Scripts\Python\Web_automation\chromedriver.exe"
+)
+driver.maximize_window()
+
+print();print("--- Test started ---")
+
+# Webpages to test
+
+Hangfire      = 'https://www.seleniumeasy.com/test/basic-checkbox-demo.html'
+BatchExplorer = 'https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html'
+
+
+# Loop to select/unselect boxes
+
+def checkbox():
+    driver.find_element_by_xpath("//*[@id='isAgeSelected']").click(); time.sleep(0.5)
+    driver.find_element_by_xpath("//*[@id='check1']").click(); time.sleep(0.5)
+    driver.find_element_by_xpath("//*[@id='isAgeSelected']").click(); time.sleep(0.5)
+    driver.find_element_by_xpath("//*[@id='check1']").click(); time.sleep(0.5)
+
+while True:
+    driver.get(Hangfire);time.sleep(1)
+    for i in range(2):
+        checkbox()
+    driver.refresh()
+    driver.get(BatchExplorer);time.sleep(1)
+    driver.find_element_by_xpath("//*[@id='normal-btn-success']").click();time.sleep(1) # aka Refresh button in BE
+    SyncIcon = "/html/body/div[2]/div/div[2]/div[1]/div/div/div[2]/a"    # Sync icon is not present in BE
+    #SyncIcon = "/html/body/div[2]/div/div[2]/div/div[2]/div[2]"          # Sync icon is present in BE
+
+    try:
+        driver.find_element_by_xpath(SyncIcon)
+        print();print(
+            "The button is located");time.sleep(1)
+        break
+    except NoSuchElementException:
+        print("The button is not located")
+        pass
+print("--- Test finished ---")
+driver.close(); print()
+
+
