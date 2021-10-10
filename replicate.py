2d94998cec9bebbe1713881d793c2da6c2e807cd
usersstef <62706574+usersstef@users.noreply.github.com>
2021-10-10 13:26:44 +0300

    Update replicate.py

--- replicate.py
+++ replicate.py
@@ -5,7 +5,16 @@
 
 import shutil
 
-for i in range(10):
+while True:
+    try:
+        num=int(input("Enter the amount of files to generate: "))
+        break;
+    except ValueError:
+        print(); print ("Invalid input, please enter only digits"); print()
+
+for i in range(num):
     shutil.copyfile('53_CBank_.wav','53_CBank_'+str(i+1)+'.wav')
+print(); print ("There are",num,"files generated")
+input("")
 
  
