ca6fe8418ff8ead675317c95a4888fd63c059aff
usersstef <62706574+usersstef@users.noreply.github.com>
2022-07-23 14:13:47 +0300

    Update csv_extracter.py

--- csv_extracter.py
+++ csv_extracter.py
@@ -12,7 +12,7 @@ with open(src_file, newline = '') as infh:  # src file
     reader = csv.reader(infh)
     for row in reader:
         if row[0] == index_0 and row[9] == index_9_link1:
-            # print(row[0], row[1], row[2], row[3]) # prin row by index
+            # print(row[0], row[1], row[2], row[3]) # prin row by index: index 0 is cell 1, index 1 is cell 2, index 2 is cell 3 and so on ...
             print(",".join(row))
         if row[0] == index_0 and row[9] == index_9_link2:
             print(",".join(row))
