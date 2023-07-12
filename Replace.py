747fe6070569993591b3432d7cf95f0b6a4aeb4d
usersstef <62706574+usersstef@users.noreply.github.com>
2023-07-12 10:18:52 +0300

    Add files via upload

---
+++ Replace.py
@@ -0,0 +1,19 @@
+# Generate files with incremented names, just copy the script in the same folder with file to copy,
+# and in line 15 edit the filename by your needs
+
+import os
+
+seq = input("Enter the sequence to be replaced: ")
+
+counter = 0
+path = r"C:\Python\Filename_replacer"
+files = [] 
+for file_name in os.listdir (path):
+    if "1_ID" in file_name:
+        old_name = file_name
+        new_name = file_name.replace ("1_ID", seq)
+        counter += 1
+        files.append (new_name)
+        os.rename (old_name, new_name)
+print (counter)
+print (files)
