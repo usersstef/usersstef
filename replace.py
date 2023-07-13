83e197b1df01be08a17d27659df597b7ac9c1bb0
usersstef <62706574+usersstef@users.noreply.github.com>
2023-07-13 23:09:54 +0300

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,5 +1,5 @@
-# Generate files with incremented names, just copy the script in the same folder with file to copy,
-# and in line 15 edit the filename by your needs
+# Copy the script in the same folder with file to copy,
+# In line 15 edit the filename by your needs
 
 import os
 
