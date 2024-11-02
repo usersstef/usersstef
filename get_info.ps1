4e527377c6bfd46e79b2d5d727d3ac0a19e3ad34
usersstef <62706574+usersstef@users.noreply.github.com>
2024-11-02 17:31:36 +0200

    Delete get_info.ps1

--- get_info.ps1
+++
@@ -1,9 +0,0 @@
-
-$env:computername
-$env:username
-getmac
-ipconfig
-cmd /c manage-bde -protectors -get c:; cmd /c manage-bde -protectors -get d:
-echo " "
-echo **********************************************************************************
-
