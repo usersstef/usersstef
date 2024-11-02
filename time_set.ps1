d4c1a19510e84d7522a832cacd02fb037a1879e8
usersstef <62706574+usersstef@users.noreply.github.com>
2024-11-02 17:44:03 +0200

    Delete time_set.ps1

--- time_set.ps1
+++
@@ -1,8 +0,0 @@
-
-echo " "
-$env:computername
-$env:username
-echo " "
-Get-Date -Format g #get_current_time
-cmd /c net time \\server1 /set /y
-echo *********************************************************
