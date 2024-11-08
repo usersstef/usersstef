99722211dadf75f1b9d9ae4477c59eb4b61aa6f8
usersstef <62706574+usersstef@users.noreply.github.com>
2024-11-08 17:42:52 +0200

    Add files via upload

--- replace.py
+++ replace.py
@@ -1,10 +1,17 @@
 
-# In line 12 & 14 edit the filename by your needs
+import shutil, glob, os
 
-import os
+file_src = r"D:/Python/Filename_replacer/ApexVO"
+# file_src = r"D:/Python/Filename_replacer/FCAudio"
+src = r"D:/Python/Filename_replacer"
+dst = r"//mad-fs1/IDS/Engineering/QATeam/Stef1"
 
 print()
-seq = input("Enter a string sequence to replace with (Ex: 7_ID): ")
+sequence = input("Enter a string sequence to replace with (Ex: 7_ID): ")
+files = glob.iglob(os.path.join(file_src, "*.wav"))
+for file in files:
+    if os.path.isfile(file):
+        shutil.copy2(file, src)
 
 counter = 0
 path = r"D:\Python\Filename_replacer"
@@ -12,11 +19,16 @@ files = []
 for file_name in os.listdir (path):
     if "1_ID" in file_name:
         old_name = file_name
-        new_name = file_name.replace ("1_ID", seq)
+        new_name = file_name.replace ("1_ID", sequence)
         counter += 1
         files.append (new_name)
         os.rename (old_name, new_name)
+# print(), print (*files, sep = '\n')
 
-print(), print (*files, sep = '\n')
-print(); print("There are", counter, "files renamed")
+files = glob.iglob(os.path.join(src, "*.wav"))
+for file in files:
+    if os.path.isfile(file):
+        shutil.move(file, dst)
+
+print(); print("There are", counter, "files renamed and copied to", dst, "location")
 
