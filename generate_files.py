62043756e5fe18f84d5564f69d1719a8481b51ec
usersstef <62706574+usersstef@users.noreply.github.com>
2024-11-08 18:14:59 +0200

    Add files via upload

--- generate_files.py
+++ generate_files.py
@@ -4,8 +4,15 @@
 
 import shutil, glob, os
 
+# file_src = r"D:/Python/Filename_generator/ApexVO"  # Apex VO file path
+file_src = r"D:/Python/Filename_generator/FCAudio"  # FC Audio file path
 src = r"D:/Python/Filename_generator"
 dst = r"//mad-fs1/IDS/Engineering/QATeam/Stef1"
+
+files = glob.iglob(os.path.join(file_src, "*.wav"))
+for file in files:
+    if os.path.isfile(file):
+        shutil.copy2(file, src)
 
 print()
 while True:
@@ -15,18 +22,19 @@ while True:
     except ValueError:
         print(); print("Invalid input, please enter only digits"); print()
         # break;
+
 for i in range(num):
-	shutil.copyfile('6_ID_GER_DE.wav','6_ID'+str(i+1)+'_GER_DE.wav')
-	#shutil.copyfile('3_FBank_.wav','3_FBank_'+str(i+1)+'.wav')
+	# shutil.copyfile('1_ID_GER_DE.wav','1_ID'+str(i+1)+'_GER_DE.wav')  #  ApexVO format
+	shutil.copyfile('1_FBank_.wav','1_FBank_'+str(i+1)+'.wav')         # FC Audio format
 
 #shutil.copyfile(src + filename, dst + filename) # this line is to copy only one file
 
 files = glob.iglob(os.path.join(src, "*.wav"))
 for file in files:
     if os.path.isfile(file):
-        shutil.copy2(file, dst)
+        shutil.move(file, dst)
 print()
-print("There are", num, "files generated and copied to share location, press Enter key to exit")
-input("")
+print("There are", num, "files generated and copied to", dst, "location")
+#input("")
 
 
